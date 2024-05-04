18ee190026dec06cc34a60cae703640b39c371d2
Franco Köhler <kohler1395@gmail.com>
2024-05-04 18:27:12 +0200

    Urls

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const producto = [
   {
     id: "cyber",
     title: "Cyberpunk 2077",
-    imageUrl: "imagenes/CYBER.jpeg",
+    imageUrl: "./imagenes/CYBER.jpeg",
     content:
       "Cyberpunk 2077 is an open-world adventure and action RPG set in the dark future of Night City, a dangerous megacity obsessed with power, glamour, and relentless body modifications.",
     price: "70$",
@@ -15,7 +15,7 @@ const producto = [
   {
     id: "fc24",
     title: "EA SPORTS FC™ 24",
-    imageUrl: "imagenes/FC24.jpg",
+    imageUrl: "./imagenes/FC24.jpg",
     content:
       "EA SPORTS FC™ 24 welcomes you to The World's Game: the most authentic football experience to date with HyperMotionV, PlayStyles optimized by Opta, and the enhanced Frostbite™ engine.",
     price: "30$",
@@ -27,7 +27,7 @@ const producto = [
   {
     id: "gow",
     title: "God of War: Ragnarök",
-    imageUrl: "imagenes/GOW2.jpg",
+    imageUrl: "./imagenes/GOW2.jpg",
     content:
       "Kratos has left behind his vengeance against the gods of Olympus and now lives as a man in the domains of the Norse gods and monsters. In this cruel and relentless world, he must fight to survive... and teach his son to do the same.",
     price: "60$",
@@ -39,7 +39,7 @@ const producto = [
   {
     id: "spiderman",
     title: "Spiderman 2",
-    imageUrl: "imagenes/spiderman2.jpg",
+    imageUrl: "./imagenes/spiderman2.jpg",
     content:
       "In Marvel's Spider-Man 2, Peter Parker's life collides with that of Spider-Man in an original action-packed story. Step into the shoes of a seasoned Peter Parker who has honed his skills in fighting crime and villains in Marvel's New York.",
     price: "50$",
@@ -51,7 +51,7 @@ const producto = [
   {
     id: "thefinals",
     title: "The Finals",
-    imageUrl: "imagenes/thefinals.jpg",
+    imageUrl: "./imagenes/thefinals.jpg",
     content:
       "Join THE FINALS, the world-famous free combat game show! Fight side by side with your teammates in virtual arenas that you can modify, leverage, and even destroy.",
     price: "10$",
@@ -63,7 +63,7 @@ const producto = [
   {
     id: "wz",
     title: "Call of Duty:Warzone",
-    imageUrl: "imagenes/Warzone2.jpeg",
+    imageUrl: "./imagenes/Warzone2.jpeg",
     content:
       "We welcome you to Call of Duty®: Warzone™, a massive free combat arena that now includes Urzikistan, Rebirth Island, and Fortune's Keep.",
     price: "10$",
@@ -75,7 +75,7 @@ const producto = [
   {
     id: "dg",
     title: "Days Gone",
-    imageUrl: "imagenes/daysgone.jpg",
+    imageUrl: "./imagenes/daysgone.jpg",
     content:
       "Days Gone is set in post-apocalyptic Oregon two years after the start of a pandemic that turned a portion of humanity into vicious zombie-like creatures.",
     price: "30$",
@@ -87,7 +87,7 @@ const producto = [
   {
     id: "2k24",
     title: "2K24",
-    imageUrl: "imagenes/2K24.jpg",
+    imageUrl: "./imagenes/2K24.jpg",
     content:
       "Experience basketball culture in NBA 2K24. Enjoy action-packed gameplay and limitless customization options for your MyPLAYER in MyCAREER.",
     price: "40$",
@@ -99,7 +99,7 @@ const producto = [
   {
     id: "ds",
     title: "Demon Souls",
-    imageUrl: "imagenes/DemonsSouls.jpg",
+    imageUrl: "./imagenes/DemonsSouls.jpg",
     content:
       "Demon's Souls is set in Boletaria, a kingdom consumed by a dark being called the Old One, following its release through the use of forbidden Soul Arts.",
     price: "30$",
@@ -111,7 +111,7 @@ const producto = [
   {
     id: "ffxvi",
     title: "Final Fantasy XVI",
-    imageUrl: "imagenes/FF16.jpeg",
+    imageUrl: "./imagenes/FF16.jpeg",
     content:
       "Final Fantasy XVI is set in the twin continents of Valisthea, currently divided between six nations who hold power through access to magical Crystals and Dominants, humans who act as hosts for each nation's Eikon.",
     price: "70$",
@@ -123,7 +123,7 @@ const producto = [
   {
     id: "returnal",
     title: "Returnal",
-    imageUrl: "imagenes/returnal.jpg",
+    imageUrl: "./imagenes/returnal.jpg",
     content:
       "Returnal is a third-person shooter featuring roguelike elements and falling under the psychological horror genre.",
     price: "30$",
@@ -135,7 +135,7 @@ const producto = [
   {
     id: "ghost",
     title: "Ghost of Tsushima",
-    imageUrl: "imagenes/ghost.jpg",
+    imageUrl: "./imagenes/ghost.jpg",
     content:
       "Ghost of Tsushima is an action-adventure where the player controls Jin Sakai, a samurai on a quest to protect Tsushima Island during the first Mongol invasion of Japan.",
     price: "30$",
